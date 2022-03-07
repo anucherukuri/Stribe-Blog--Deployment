aa6c4e0c21c3003911097821bd47bf8b58dce71a
Anusha <anushach514@gmail.com>
2022-03-07 15:49:23 +0000

    .env updated

--- src/server.js
+++ src/server.js
@@ -30,6 +30,34 @@ server.use(express.json());
 
 server.use(express.static(publicDirectory));
 
+//************************************** CORS ********************************************* */
+
+const whitelist = [process.env.FE_DEV_URL, process.env.FE_PROD_URL]
+
+server.use(express.static(publicFolderPath))
+server.use(loggerMiddleware) // Global middleware
+server.use(
+  cors({
+    origin: function (origin, next) {
+      // cors is a global middleware --> for each and every request we are going to be able to read the current origin value
+      console.log("ORIGIN: ", origin)
+      if (!origin || whitelist.indexOf(origin) !== -1) {
+        // indexOf returns -1 when the element is NOT in the array
+        console.log("Origin allowed!")
+        next(null, true) // origin is in the whitelist --> move next with no errors
+      } else {
+        console.log("Origin NOT allowed!")
+        next(new Error("CORS ERROR!")) // origin is NOT in the whitelist --> trigger an error
+      }
+    },
+  })
+)
+// server.use(fakeAuthMiddleware) // Global middleware
+server.use(express.json()) // If you don't add this configuration to our server (BEFORE the endpoints), all requests' bodies will be UNDEFINED
+
+
+
+//************************************** END POINTS ********************************************* */
 server.use("/authors", authorsRouter);
 
 server.use("/blogs", blogsRouter);
